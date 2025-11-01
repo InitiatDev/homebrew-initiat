be6016c6ee7abe108037e25836dd945d0a0f9d55
GitHub Action <action@github.com>
2025-11-01 22:14:22 +0000

    Update initiat to v0.7.0

--- Formula/initiat.rb
+++ Formula/initiat.rb
@@ -1,19 +1,19 @@
 class Initiat < Formula
   desc "The Developer Experience Platform that eliminates onboarding friction"
   homepage "https://initiat.dev"
-  url "https://github.com/InitiatDev/initiat-cli/releases/download/v0.6.1/initiat-darwin-amd64.tar.gz"
-  sha256 "b4c017ecffa9b206767421c33c8963c31e6bcec7fcec732ecd796e832200f3a3"
+  url "https://github.com/InitiatDev/initiat-cli/releases/download/v0.7.0/initiat-darwin-amd64.tar.gz"
+  sha256 "effacc6ce1c34caa19cc602a8a19bd264df3179367013c99cc9acc5c89bb99f0"
   license "AGPL-3.0"
-  version "v0.6.1"
+  version "v0.7.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/InitiatDev/initiat-cli/releases/download/v0.6.1/initiat-darwin-amd64.tar.gz"
-      sha256 "b4c017ecffa9b206767421c33c8963c31e6bcec7fcec732ecd796e832200f3a3"
+      url "https://github.com/InitiatDev/initiat-cli/releases/download/v0.7.0/initiat-darwin-amd64.tar.gz"
+      sha256 "effacc6ce1c34caa19cc602a8a19bd264df3179367013c99cc9acc5c89bb99f0"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/InitiatDev/initiat-cli/releases/download/v0.6.1/initiat-darwin-arm64.tar.gz"
-      sha256 "e188a87b2cff09908e638eb68eac772c6ead84d64e2b9bc8955e1ac9778ee19c"
+      url "https://github.com/InitiatDev/initiat-cli/releases/download/v0.7.0/initiat-darwin-arm64.tar.gz"
+      sha256 "7d6a511da43b067d0d82dc41d0ad04a0999f7c9c04996dd2f9b58a426bd3b231"
     end
   end
 
